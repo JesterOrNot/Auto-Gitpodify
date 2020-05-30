e246cf0ddd1feeab4d6c9050ce9fb30fca5e19a6
Sean Hellum <seanhellum45@gmail.com>
2020-05-30 13:33:59 +0000

    make adding checkers easy using FP

--- languages.go
+++ languages.go
@@ -2,35 +2,19 @@ package main
 
 func CheckAll(files []string) {
 	waitGroup.Add(3)
-	go checkGo(files)
-	go checkRust(files)
-	go checkYarn(files)
+	go makeChecker("go.mod", "go")(files)
+	go makeChecker("Cargo.toml", "rust")(files)
+	go makeChecker("yarn.lock", "yarn")(files)
 	waitGroup.Wait()
 }
 
-func checkRust(files []string) {
-	defer waitGroup.Done()
-	for _, file := range files {
-		if file == "Cargo.toml" {
-			Languages = append(Languages, "rust")
-		}
-	}
-}
-
-func checkYarn(files []string) {
-	defer waitGroup.Done()
-	for _, file := range files {
-		if file == "yarn.lock" {
-			Languages = append(Languages, "yarn")
-		}
-	}
-}
-
-func checkGo(files []string) {
-	defer waitGroup.Done()
-	for _, file := range files {
-		if file == "go.mod" {
-			Languages = append(Languages, "go")
+func makeChecker(targetFileName string, language string) func(files []string) {
+	return func(files []string) {
+		defer waitGroup.Done()
+		for _, file := range files {
+			if file == targetFileName {
+				Languages = append(Languages, language)
+			}
 		}
 	}
 }
